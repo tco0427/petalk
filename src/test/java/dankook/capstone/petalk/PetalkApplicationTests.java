bc10e74bd50c4ee1654e78f1fdb40dc2e1fdeb6a
tco0427 <a29661498@gmail.com>
2021-03-30 19:35:26 +0900

    Initial commit

---
+++ src/test/java/dankook/capstone/petalk/PetalkApplicationTests.java
@@ -0,0 +1,13 @@
+package dankook.capstone.petalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
