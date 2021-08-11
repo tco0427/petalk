128da772ce9a960a09d34d2df86af4280dc55a20
Donggyu Kim <a29661498@gmail.com>
2021-08-11 21:54:24 +0900

    feature: add CommunityController 게시글 수정 기능 구현
    * CommunityController 게시글 수정 기능 구현

--- src/main/java/dankook/capstone/petalk/data/ResponseMessage.java
+++ src/main/java/dankook/capstone/petalk/data/ResponseMessage.java
@@ -7,4 +7,5 @@ public class ResponseMessage {
     public static final String COMMUNITY_CREATION_FAIL = "게시글 생성 실패";
     public static final String NOT_FOUND_USER = "회원 정보 조회 실패";
     public static final String NOT_FOUND_PET = "펫 정보 조회 실패";
+    public static final String NOT_FOUND_COMMUNITY = "게시글 정보 조회 실패";
 }
