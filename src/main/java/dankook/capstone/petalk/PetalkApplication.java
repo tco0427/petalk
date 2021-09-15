677ebde8d0f40f8ce41671201b115d77653ce060
Donggyu Kim <a29661498@gmail.com>
2021-09-15 11:22:30 +0900

    feature: s3 설정 및 S3Uploader 구현 완료
    * S3 관련 설정 추가
    * S3Uploader.java 작성 완료

--- src/main/java/dankook/capstone/petalk/PetalkApplication.java
+++ src/main/java/dankook/capstone/petalk/PetalkApplication.java
@@ -2,12 +2,23 @@ package dankook.capstone.petalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class PetalkApplication {
+	public static final String APPLICATION_LOCATIONS = "spring.config.location=" +
+			"classpath:application.yml," +
+			"classpath:aws.hml";
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(PetalkApplication.class, args);
+//	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(PetalkApplication.class, args);
+		new SpringApplicationBuilder(PetalkApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 }
