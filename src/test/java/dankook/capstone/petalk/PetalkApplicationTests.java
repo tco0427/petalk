15da4b1e75bc70a60abf61d184d8cbb18fecf0dd
Donggyu Kim <a29661498@gmail.com>
2021-09-24 15:46:32 +0900

    feature: test PetalkApplicationTests 수정

--- src/test/java/dankook/capstone/petalk/PetalkApplicationTests.java
+++ src/test/java/dankook/capstone/petalk/PetalkApplicationTests.java
@@ -3,7 +3,7 @@ package dankook.capstone.petalk;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PetalkApplicationTests {
 
 	@Test
