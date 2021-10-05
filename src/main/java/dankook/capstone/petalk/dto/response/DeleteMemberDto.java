4125b08a3c034cff3969943b02f11ea685e1792d
Donggyu Kim <a29661498@gmail.com>
2021-10-05 22:04:17 +0900

    feature: MemberDto 분할

---
+++ src/main/java/dankook/capstone/petalk/dto/response/DeleteMemberDto.java
@@ -0,0 +1,10 @@
+package dankook.capstone.petalk.dto.response;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class DeleteMemberDto {
+    private Long id;
+}
