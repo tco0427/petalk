34d52cbe026dd5e564e4e3e9a5fd68ffe822b6c4
Donggyu Kim <a29661498@gmail.com>
2021-11-23 13:29:00 +0900

    fix: 댓글 DTO createdDate 추가

--- src/main/java/dankook/capstone/petalk/dto/response/CommentDto.java
+++ src/main/java/dankook/capstone/petalk/dto/response/CommentDto.java
@@ -3,12 +3,16 @@ package dankook.capstone.petalk.dto.response;
 import dankook.capstone.petalk.entity.Comment;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class CommentDto {
     private String writer;
+    private LocalDateTime createdDate;
     private String content;
 
     public CommentDto(Comment comment){
+        this.createdDate = comment.getCreatedDate();
         this.writer = comment.getMember().getNickname();
         this.content = comment.getContent();
     }
